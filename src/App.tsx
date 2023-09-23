dfdff536308bcffadba11fb340a8373771589c7b
Carlos Tizirai <81642896+karlzw@users.noreply.github.com>
2023-09-23 21:47:04 +0200

    chore: setups tech stack

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello Carlos</h1>;
+}
+
+export default App;
---
+++ src/config/firebase.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDS0700_A9fS83vSKl5tp53DzWmgn5Qm00",
+  authDomain: "penny-pincher-3d29d.firebaseapp.com",
+  projectId: "penny-pincher-3d29d",
+  storageBucket: "penny-pincher-3d29d.appspot.com",
+  messagingSenderId: "287704469638",
+  appId: "1:287704469638:web:024b75b414ea837f6fc107",
+  measurementId: "G-WWRG5HP0VF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
