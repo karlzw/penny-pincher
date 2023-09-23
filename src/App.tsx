8191026ee7895c436dd550ee18933db01f5ac120
Carlos Tizirai <81642896+karlzw@users.noreply.github.com>
2023-09-23 23:31:19 +0200

    feat: add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
-function App() {
-  return <h1>Hello Carlos</h1>;
-}
+import { ThemeProvider } from "@emotion/react";
+import { LightMode, DarkMode } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  styled,
+  Typography,
+  CssBaseline,
+} from "@mui/material";
+import { red } from "@mui/material/colors";
+import { useThemeMode } from "./customhooks/useThemeMode";
 
-export default App;
+const CustomButton = styled(Button)`
+  background: ${red[400]};
+  color: white;
+
+  &:hover {
+    /* color: ${red[500]}; */
+    background: ${red[600]};
+  }
+`;
+
+export default function App() {
+  const { theme, changeColorMode } = useThemeMode();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Box>
+        <Typography>Hello Carlos</Typography>
+        <CustomButton>This is a button</CustomButton>
+
+        {theme.palette.mode === "dark" ? (
+          <LightMode onClick={changeColorMode} sx={{ cursor: "pointer" }} />
+        ) : (
+          <DarkMode onClick={changeColorMode} sx={{ cursor: "pointer" }} />
+        )}
+      </Box>
+    </ThemeProvider>
+  );
+}
