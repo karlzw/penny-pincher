30d9421c84f4eff5a36358c7c88a947ab3052384
Carlos Tizirai <81642896+karlzw@users.noreply.github.com>
2023-09-24 21:20:05 +0200

    style: initial nav design

--- src/config/Router.tsx
+++ src/config/Router.tsx
@@ -1,8 +1,12 @@
 import { Box } from "@mui/material";
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import ProtectedRoutes from "../components/ProtectedRoutes";
+import Analytics from "../pages/Analytics";
 import { Dashboard } from "../pages/Dashboard";
 import { LandingPage } from "../pages/LandingPage";
+import PageNotFound from "../pages/PageNotFound";
+import Settings from "../pages/Settings";
+import Transactions from "../pages/Transactions";
 
 export function Router() {
   return (
@@ -11,8 +15,11 @@ export function Router() {
         <Route path="/" element={<LandingPage />} />
         <Route element={<ProtectedRoutes />}>
           <Route index path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/settings" element={<Settings />} />
         </Route>
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Box>
   );
