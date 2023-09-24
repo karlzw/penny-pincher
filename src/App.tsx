7a1c8d32dd5ee000feeaf60c81015b79b21e27e3
Carlos Tizirai <81642896+karlzw@users.noreply.github.com>
2023-09-24 14:08:47 +0200

    feat: add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import ToggleTheme from "./components/ToggleTheme";
 import { Router } from "./config/Router";
 import { Theme } from "./config/Theme";
 
 export default function App() {
-  const { theme, changeColorMode } = Theme();
+  const { theme } = Theme();
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ToggleTheme theme={theme} changeColorMode={changeColorMode} />
       <Router />
     </ThemeProvider>
   );
--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,9 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  FacebookAuthProvider,
-  GoogleAuthProvider,
-  getAuth,
-} from "firebase/auth";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDS0700_A9fS83vSKl5tp53DzWmgn5Qm00",
@@ -18,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const facebookProvider = new FacebookAuthProvider();
